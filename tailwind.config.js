0d4fb65185ad7366a15d5bc55d75e1d446fd4c11
Jason Miller <jasonmiller.dev@gmail.com>
2022-04-30 20:19:24 +1000

    Second commit: fixed tailwind config issue...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: '[./*.{html}]',
+  content: ['./index.html'],
   theme: {
     extend: {
       colors: {
@@ -10,11 +10,12 @@ module.exports = {
         'secondary': "#9191A4",
         'badge': "#3F3F51",
         "input-border": "#565666",
-        'input': "#2A2A35"
+        'input': "#2A2A35",
       },
       fontFamily: {
-        'poppins': ["'Poppins'", "sans-serif"]
+        poppins: ["'Poppins'", "sans-serif"],
       },
     },
   },
 };
+
