c362ddda0f366158a5b3537addf920fbf9fbb02e
Jason Miller <jasonmiller.dev@gmail.com>
2022-05-01 18:47:59 +1000

    second commit: minor personalisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
     extend: {
       colors: {
-        'body': "#17171F",
+        body: "#17171F",
         "selected-text": "#A3A3FF",
-        'theme': "#3F3FFF",
-        'nav': "#404053",
-        'secondary': "#9191A4",
-        'badge': "#3F3F51",
+        theme: "#3F3FFF",
+        nav: "#404053",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
         "input-border": "#565666",
-        'input': "#2A2A35",
+        input: "#2A2A35",
       },
       fontFamily: {
         poppins: ["'Poppins'", "sans-serif"],
@@ -18,4 +18,3 @@ module.exports = {
     },
   },
 };
-
